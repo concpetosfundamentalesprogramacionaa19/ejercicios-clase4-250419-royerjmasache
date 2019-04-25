d23d3c0fa6d64b2aa301f09da3338e8e69f2ab00
royerjmasache <josethxm@gmail.com>
2019-04-25 11:32:58 -0500

    Ejercicios de estructuras condicionales e importanción de paquetes en Java

---
+++ Ejercicios/src/ejercicios/SeleccionSimple.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios;
+
+import java.util.Scanner;
+import paquetedos.*;
+/**
+ *
+ * @author royerjmasache
+ */
+public class SeleccionSimple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Estructura condicional
+        Scanner entrada = new Scanner(System.in);
+        int calificacion = 0;
+        int calificacion2 = 0;
+        String miMensaje = Operacion.mensaje;
+        // Ingreso de datos
+        System.out.println("Ingrese la primera calificación");
+        calificacion = entrada.nextInt();
+        // Estructura condicional
+        if(calificacion >= 85){
+            System.out.printf("%s %d\n", miMensaje, calificacion);
+        }
+        // Ingreso de datos
+        System.out.println("Ingrese la segunda calificación");
+        calificacion2 =  entrada.nextInt();
+        // Estructura condicional
+        if(calificacion2 >= 85){
+            System.out.printf("%s %d", miMensaje, calificacion2);
+        }
+    }
+}
---
+++ Ejercicios/src/paquetedos/Operacion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquetedos;
+
+/**
+ *
+ * @author royerjmasache
+ */
+public class Operacion {
+    // Se declara una variable estática y se presenta un mensaje
+    public static String mensaje = ("Usted está aprobado con: ");
+}
